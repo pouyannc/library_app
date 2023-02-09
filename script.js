eda568236cf3c6b936584c3bbf3839258df54348
Pouyan <pouyannc@gmail.com>
2023-02-09 16:20:40 -0500

    Base HTML and CSS for navbar

---
+++ script.js
@@ -0,0 +1,14 @@
+let myLibrary = [handmaidsTale];
+
+function Book(title, author, page, read) {
+  this.title = title
+  this.author = author
+  this.page = page
+  this.read = read
+}
+
+function addBookToLibrary() {
+  
+}
+
+let handmaidsTale = new Book("The Handmaid's Tale", "Margaret Atwood", 311, false)
