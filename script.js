2e30538a2304b7da2b6db6d99daa595df245112c
Pouyan <pouyannc@gmail.com>
2023-02-09 20:54:13 -0500

    Dynamic book panels html

--- script.js
+++ script.js
@@ -1,14 +1,49 @@
-let myLibrary = [handmaidsTale];
+const myLibrary = [];
 
 function Book(title, author, page, read) {
-  this.title = title
-  this.author = author
-  this.page = page
-  this.read = read
+  this.title = title;
+  this.author = author;
+  this.page = page;
+  this.read = read;
 }
 
 function addBookToLibrary() {
-  
+  myLibrary.push(new Book('"The Handmaid\'s Tale"', 'Margaret Atwood', `Pages: ${311}`, false));
 }
 
-let handmaidsTale = new Book("The Handmaid's Tale", "Margaret Atwood", 311, false)
+const libDisplay = document.querySelector('.collection');
+const addButton = document.querySelector('.add-button');
+
+addBookToLibrary();
+
+myLibrary.forEach((book, i) => {
+  const bookPanel = libDisplay
+    .appendChild(
+      Object.assign(
+        document.createElement('div'),
+        { className: 'book-panel' },
+      ),
+    );
+
+  Object.values(book).forEach((prop) => {
+    bookPanel.appendChild(
+      Object.assign(
+        document.createElement('div'),
+        {
+          className: 'book-info',
+          innerHTML: prop,
+        },
+      ),
+    );
+  });
+
+  bookPanel.appendChild(
+    Object.assign(
+      document.createElement('div'),
+      {
+        className: 'remove-button',
+        innerHTML: 'Remove',
+      },
+    ),
+  );
+});
