9e35eea3a0772f00868520fd6911642f24fbfb32
Pouyan <pouyannc@gmail.com>
2023-02-11 19:34:46 -0500

    Toggle read status option

---
+++ README.md
@@ -0,0 +1,2 @@
+# Library collection app
+
--- script.js
+++ script.js
@@ -8,9 +8,20 @@ function Book(title, author, page, read) {
   this.read = read;
 }
 
+function toggleRead(e) {
+  const readText = e.target.textContent;
+  if (readText === 'Read ✔') {
+    e.target.textContent = 'Not read';
+  } else if (readText === 'Not read') {
+    e.target.textContent = 'Read ✔';
+  }
+}
+
 function removeBook(e) {
   const book = e.srcElement.parentElement.parentElement;
+  myLibrary.splice(book.dataset.index, 1);
   book.remove();
+  document.querySelectorAll('.book-panel').forEach((p, i) => { p.dataset.index = i; });
 }
 
 function createBookPanel(book) {
@@ -19,17 +30,20 @@ function createBookPanel(book) {
       Object.assign(document.createElement('div'), { className: 'book-panel' }),
     );
 
+  bookPanel.setAttribute('data-index', `${myLibrary.length - 1}`);
+
   const infoContainer = bookPanel
     .appendChild(
       Object.assign(document.createElement('div'), { className: 'info-container' }),
     );
 
-  Object.values(book).forEach((prop) => {
+  Object.values(book).forEach((prop, i) => {
     infoContainer.appendChild(
       Object.assign(
         document.createElement('div'),
         {
           className: 'book-info',
+          id: `info-line-${i + 1}`,
           innerHTML: prop,
         },
       ),
@@ -53,6 +67,7 @@ function createBookPanel(book) {
     ),
   );
 
+  document.querySelectorAll('#info-line-4').forEach((b) => b.addEventListener('click', toggleRead));
   document.querySelectorAll('.remove-button').forEach((b) => b.addEventListener('click', removeBook));
 }
 
@@ -60,22 +75,19 @@ function addBookToLibrary(bookObj) {
   myLibrary.push(bookObj);
 }
 
-function createBook() {
+function createBook(event) {
+  event.preventDefault();
+
   const title = document.getElementById('title').value;
   const author = document.getElementById('author').value;
   const pages = document.getElementById('pages').value;
-  const read = document.getElementById('read').value;
-  const book = new Book(`"${title}"`, author, `${pages} Pages`, read);
+  const read = document.getElementById('read').checked;
+  const book = new Book(`"${title}"`, author, `Pages: ${pages}`, read ? 'Read ✔' : 'Not read');
   document.querySelector('.book-form-container').reset();
   addBookToLibrary(book);
   createBookPanel(book);
 }
 
-addBookToLibrary();
-
 document.getElementById('open-form').addEventListener('click', () => { document.querySelector('.book-form').style.display = 'flex'; });
 document.getElementById('close-btn').addEventListener('click', () => { document.querySelector('.book-form').style.display = 'none'; });
-document.getElementById('submit-btn').addEventListener('click', createBook);
-
-// to do: remove book from myLibrary array in remove book function,
-// don't allow form to be submitted with empty spaces
+document.querySelector('.book-form-container').addEventListener('submit', createBook);
